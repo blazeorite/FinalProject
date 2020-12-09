d37ab94c315c36f5f1e62dfdcec9a2d29a836336
Cristian Barco <c.barco@outlook.com>
2020-12-08 22:04:28 -0500

    website

--- Website/urls.py
+++ Website/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('investment', views.investment, name="investment"),
     path('login', views.login, name="login"),
     path('register', views.register, name="register"),
-    path('budget-dashboard/', include('budget.urls'))
+    path('budget-dashboard/', views.budget_dashboard,),
+    path('budget-dashboard/add', views.budget_dashboard_add, name="budget-dashboard-add"),
+    path('budget-dashboard/delete', views.budget_dashboard_delete, name="budget-dashboard-delete"),
+    path('budget-dashboard/update', views.budget_dashboard_update, name="budget-dashboard-update"),
     #testing23#
     ## cristian barco
 ]
--- Website/views.py
+++ Website/views.py
@@ -20,3 +20,19 @@ def login(request):
 
 def register(request):
     return HttpResponse("Hello, register")
+
+
+def budget_dashboard(request):
+    return HttpResponse("Hello, budgetdashboard")
+
+
+def budget_dashboard_add(request):
+    return HttpResponse("Hello, budgetdashboard-add")
+
+
+def budget_dashboard_delete(request):
+    return HttpResponse("Hello, budgetdashboard-delete")
+
+
+def budget_dashboard_update(request):
+    return HttpResponse("Hello, budgetdashboard-update")
