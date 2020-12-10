7fbe88195bb29d9110521f812a9db9e5154ccbbd
unknown <sujith0709@gmail.com>
2020-12-10 07:35:51 -0500

    fixed missing login auth

--- Website/views.py
+++ Website/views.py
@@ -97,7 +97,7 @@ def logout_view(request):
     logout(request)
     return redirect('index')
 
-
+@login_required(login_url='../login')
 def budget_dashboard_details(request, id_):
     budget = Budget.objects.get(id=id_)
     # store the benchmark numbers
